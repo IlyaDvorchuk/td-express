fae3e21e1ca8e2075a057fd5ab6a8203a0ed0af0
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-10-11 09:27:06 +0300

    1

--- src/components/forms/filter-cards/FilterCards.tsx
+++ src/components/forms/filter-cards/FilterCards.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './filter-cards.scss'
+import TwoWayRange from "../../inputs/two-range/TwoRange";
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 const FilterCards = () => {
+    const [range, setRange] = useState([0, 100]);
+
+    const handleRangeChange = (newRange: number) => {
+        // setRange(newRange);
+    };
+
     return (
         <aside className={'filter'}>
             <h3>Цена, руб</h3>
@@ -15,6 +24,16 @@ const FilterCards = () => {
                     <input className={'modalInput modalInput_light'} type="text" id="max-price"/>
                 </div>
             </div>
+            <div>
+                <Slider
+                    range
+                />
+                <div>
+                    <span>Min: {range[0]}</span>
+                    <span>Max: {range[1]}</span>
+                </div>
+            </div>
+            {/*<input className={'filter__range'} type="range"/>*/}
             <button className={'button button_light filter__button'}>Очистить всё</button>
         </aside>
     );
