b14c60569f2519aae4e316416b26b728096abe77
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-12-25 09:08:18 +0200

    fix: remove notification tabs

--- src/components/notifications/notifications-wrapper/NotificationsWrapper.tsx
+++ src/components/notifications/notifications-wrapper/NotificationsWrapper.tsx
@@ -1,8 +1,6 @@
 import React, { useState} from 'react';
 import './notifications-wrapper.scss'
 import NotificationSvg from "../../svg/NotificationSvg";
-import TechnicalSupportSvg from "../../svg/TechnicalSupportSvg";
-import CommunicationSvg from "../../svg/CommunicationSvg";
 import NotificationCard from "../notification-card/NotificationCard";
 import {useAppSelector} from "../../../hooks/redux";
 import {INotification} from "../../../models/INotification";
@@ -40,32 +38,32 @@ const NotificationsWrapper = ({notifications, setRemoveNotifications, isSeller =
                             </span>
                     {unreadCount > 0 && <div className={'unread'}/>}
                 </div>
-                {
-                    isSeller && <>
-                        <div
-                            className={`notifications-item ${activeNotification === 1 && 'active'}`}
-                            onClick={() => setActiveNotification(1)}
-                        >
-                            <div>
-                                <TechnicalSupportSvg/>
-                            </div>
-                            <span className={'notifications-item__text'}>
-                                Общение<br/>с покупателем
-                            </span>
-                        </div>
-                        <div
-                            className={`notifications-item ${activeNotification === 2 && 'active'}`}
-                            onClick={() => setActiveNotification(2)}
-                        >
-                            <div>
-                                <CommunicationSvg/>
-                            </div>
-                            <span className={'notifications-item__text'}>
-                                Общение<br/>с тех.поддержкой
-                            </span>
-                        </div>
-                    </>
-                }
+                {/*{*/}
+                {/*    isSeller && <>*/}
+                {/*        <div*/}
+                {/*            className={`notifications-item ${activeNotification === 1 && 'active'}`}*/}
+                {/*            onClick={() => setActiveNotification(1)}*/}
+                {/*        >*/}
+                {/*            <div>*/}
+                {/*                <TechnicalSupportSvg/>*/}
+                {/*            </div>*/}
+                {/*            <span className={'notifications-item__text'}>*/}
+                {/*                Общение<br/>с покупателем*/}
+                {/*            </span>*/}
+                {/*        </div>*/}
+                {/*        <div*/}
+                {/*            className={`notifications-item ${activeNotification === 2 && 'active'}`}*/}
+                {/*            onClick={() => setActiveNotification(2)}*/}
+                {/*        >*/}
+                {/*            <div>*/}
+                {/*                <CommunicationSvg/>*/}
+                {/*            </div>*/}
+                {/*            <span className={'notifications-item__text'}>*/}
+                {/*                Общение<br/>с тех.поддержкой*/}
+                {/*            </span>*/}
+                {/*        </div>*/}
+                {/*    </>*/}
+                {/*}*/}
             </div>
             <div className={'notifications-wrapper'}>
                 {notifications.map(notification => (
